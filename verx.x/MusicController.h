8c787eb759e7e3c8ad2c186e63788313d23427f8
takumi13 <38643137+takumi13@users.noreply.github.com>
2019-07-16 00:09:33 +0900

    Add files via upload

--- verx.x/MusicController.h
+++ verx.x/MusicController.h
@@ -4,16 +4,18 @@
 
 #include "alut.h"
 
-
+//--------------------------
+// 0 : START
+// 1 : PLAY
+// 2 : RESULT
+//--------------------------
 int sound_check;			//BGMの種類を決める
 bool se_pig_check;			//ブタの鳴き声の1回目を制御
-//int tmp = 0;
+float volume_BGM = 1.0f;	//BGMの音量
+float volume_PIG = 1.0f;	//ブタの鳴き声の音量
+float volume_WALL = 1.0f;	//壁に衝突したときの音量
 
-//---volume---//
-float volume_BGM = 1.0f;		//BGMの音量
-float volume_PIG = 1.0f;		//ブタの鳴き声の音量
-float volume_WALL = 1.0f;		//壁に衝突したときの音量
-float volume_GetItem = 1.0f;	//アイテムを取得したときの音量
+int tmp = 0;
 
 //---SE---//
 ALuint seCollisionWall;		//壁に衝突するSE
@@ -24,7 +26,6 @@ ALuint sePig;				//ブタの鳴き声のSE
 //---BGM---//
 ALuint bgmStart;			//スタート画面のBGM
 ALuint bgmPlay;				//プレイ画面のBGM
-ALuint bgmResult;			//リザルト画面のBGM
 
 //---souce SE---//
 ALuint sseCollisionWall;	//壁に衝突するsSE
@@ -35,7 +36,6 @@ ALuint ssePig;				//ブタの鳴き声のsSe
 //---souce BGM---//
 ALuint sbgmStart;			//スタート画面のsBGM
 ALuint sbgmPlay;			//プレイ画面のsBGM
-ALuint sbgmResult;			//リザルト画面のsBGM
 
 int countCollisionWall = 0;		//壁に衝突時のSEを連座奥再生しないように制御する
 int initCountCollision = 300;	//countがこの値を超えたら0に初期化する
